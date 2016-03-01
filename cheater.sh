0c1dd1efda39fca24f17c6f414a9e48a9ae6822b
Will Krause <admin@building-email.com>
2016-02-29 20:18:27 -0500

    just another bot commit

--- cheater.sh
+++ cheater.sh
@@ -1,3 +1,2 @@
 echo Cheater >> cheating.txt;
-cd /root/miamidevscheat && git add -A && git commit -m 'just another bot commit' && git push origin master
-;
+cd /root/miamidevscheat && git add -A && git commit -m 'just another bot commit' && git push origin master;
